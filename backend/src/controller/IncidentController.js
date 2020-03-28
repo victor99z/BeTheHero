f016e8873794f0e21a3cac10376e015373e7c42c
Victor <victor_luizbernardes5@outlook.com>
2020-03-28 06:07:36 -0300

    inc. join de ong_id em /incidents

--- backend/src/controller/IncidentController.js
+++ backend/src/controller/IncidentController.js
@@ -17,11 +17,23 @@ async function index(req, res){
 
     const { page = 1 } = req.query
 
+    const [count] = await connection('incidents').count()
+
     const data = await connection('incidents')
+        .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
         .limit(5)
         .offset((page - 1) * 5)
-        .select('*')
+        .select([
+            'incidents.*',
+            'ongs.name',
+            'ongs.email',
+            'ongs.whatsapp',
+            'ongs.city',
+            'ongs.uf'
+        ])
 
+
+    res.header('X-TOTAL-COUNT', count['count(*)'])
     return res.json(data)
 
 }
